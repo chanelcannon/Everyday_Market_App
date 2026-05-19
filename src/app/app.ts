9f9ef930ee0c0cd1e4d0f591d72905f744f1878d
chanel <chanelcannon@hotmail.com>
2026-05-19 17:08:57 -0500

    Up to Assignment 1 Step 4 HeaderComponent

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
+import {ProductsPage} from "./market/products-page/products-page";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, ProductsPage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
