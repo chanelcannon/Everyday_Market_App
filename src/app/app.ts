957ad0bc6a435b6c7c2928a89a745258ecb0dd8c
chanel <chanelcannon@hotmail.com>
2026-05-20 15:17:57 -0500

    Finished Assignment 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
-import {ProductsPage} from "./market/products-page/products-page";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, ProductsPage],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Everyday Market App');
+  protected readonly title = signal("Chanel's Everyday Market");
 }
